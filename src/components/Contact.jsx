e67a47b31709eaaa9fd543e0adbacee8a78f2a6e
noammery <noammery55@gmail.com>
2022-08-30 16:22:00 +0300

    contactdetailes add

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,24 +1,25 @@
-import {Formik} from "formik" 
+import { Formik } from "formik";
+import { useState } from "react";
 import * as Yup from "yup";
-
+import ContactDetails from "./ContactDetails";
 const Contact = () => {
-    const schema = Yup.object().shape({
-        firstName: Yup.string()
-        .required("please enter your first name"),
-        reasonOfContact: Yup.string()
-        .required("Enter your reason of contact")
-        .max(100, "maximom 100 letters"), 
-        how: Yup.string()
-        .required("choose an option")
-        })
-return(
+  const schema = Yup.object().shape({
+    firstName: Yup.string().required("please enter your first name"),
+    reasonOfContact: Yup.string()
+      .required("Enter your reason of contact")
+      .max(100, "maximom 100 letters"),
+    how: Yup.string().required("choose an option"),
+  });
+  const [items, setItems] = useState([]);
+
+  return (
     <div className="contact">
-        <h1>contact-us</h1>
-        <Formik 
-        validationSchema = {schema}
-        initialValues = {{firstName:'' , reasonOfContact:'', how: '' }}
+      <h1>contact-us</h1>
+      <Formik
+        validationSchema={schema}
+        initialValues={{ firstName: "", reasonOfContact: "", how: "" }}
         onSubmit={(values) => alert(`are you sure?` + JSON.stringify(values))}
-        >
+      >
         {({
           handleSubmit,
           handleChange,
@@ -26,8 +27,8 @@ return(
           values,
           errors,
           touched,
-        }) =>(
-        <form onSubmit={handleSubmit} noValidate> 
+        }) => (
+          <form onSubmit={handleSubmit} noValidate>
             <input
               type="text"
               name="firstName"
@@ -45,25 +46,32 @@ return(
               value={values.reasonOfContact}
               onBlur={handleBlur}
             />
-            <p>{errors.reasonOfContact && touched.reasonOfContact && errors.reasonOfContact}</p>
+            <p>
+              {errors.reasonOfContact &&
+                touched.reasonOfContact &&
+                errors.reasonOfContact}
+            </p>
             <label>How did you hear about us?</label>
-        <select
-          {...handleChange("select")}
-          onChange={handleChange}
-          >
-          <option value="">Null</option>
-          <option value="1">1</option>
-          <option value="2">2</option>
-        </select>
-        {errors.how && <p>{errors.how.message}</p>}
-            <button type="submit">submit</button>
-    
-
-        </form>)
-         }
-        </Formik>
+            <select {...handleChange("select")} onChange={handleChange}>
+              <option value="">Null</option>
+              <option value="1">1</option>
+              <option value="2">2</option>
+            </select>
+            {errors.how && <p>{errors.how.message}</p>}
+            <button
+              type="submit"
+              onClick={() =>
+                setItems([values.firstName, values.reasonOfContact])
+              }
+            >
+              submit
+            </button>
+          </form>
+        )}
+      </Formik>
+      <ContactDetails items={items} />
     </div>
-)
-}
+  );
+};
 
-export default Contact
+export default Contact;
